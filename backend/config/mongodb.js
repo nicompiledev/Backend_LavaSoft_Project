98d9d94741fda47adfe84a9ae06ba09ca79ef6e6
Johnkevin Ortega <slash2130kevin@gmail.com>
2023-05-10 16:18:49 -0500

    For crear usuarios

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -9,19 +9,20 @@ const conectarMongoDB = async () => {
       useUnifiedTopology: true,
     });
 
-/*     const {Servicio} = require("../models/Servicio.js");
+    const {Servicio} = require("../models/Servicio.js");
     const Lavadero = require("../models/lavadero.js");
 
+    for (let i = 0; i < 10; i++) {
     const lavadero1 = new Lavadero({
-      nombre: 'Lavadero 1',
-      ciudad: 'Ciudad 1',
-      direccion: 'Dirección 1',
-      telefono: '123456789',
-      correo_electronico: 'lavadero1@example.com',
-      contrasena: 'contraseña',
+      nombre: 'Lavadero' + i,
+      ciudad: 'Ciudad' + i,
+      direccion: 'Dirección' + i,
+      telefono: '123456789' + i,
+      correo_electronico: 'lavadero' + i + '@gmail.com',
+      contrasena: 'contraseña' + i,
       hora_apertura: '08:00',
       hora_cierre: '18:00',
-      imagenes: ['https://seguroserenus.com.co/wp-content/uploads/2022/09/imagen-seguro-carro1-serenus.jpg', 'https://elestimulo.com/wp-content/uploads/2015/07/carro1.jpg', 'https://d2yoo3qu6vrk5d.cloudfront.net/images/20211220091840/carro1.jpg'],
+      imagenes: ['https://plazaimperialcc.com.co/images/servicios/78/slides/medium_banner_2.jpg', 'https://elestimulo.com/wp-content/uploads/2015/07/carro1.jpg', 'https://d2yoo3qu6vrk5d.cloudfront.net/images/20211220091840/carro1.jpg'],
       espacios_de_trabajo: 5,
       ubicacion: {
         type: "Point",
@@ -40,32 +41,22 @@ const conectarMongoDB = async () => {
     const lavadero = await Lavadero.findById(lavadero1._id);
     lavadero.servicios.push(servicio1._id);
     lavadero.servicios.push(servicio2._id);
-    await lavadero.save(); */
+    await lavadero.save();
+  }
 
-/*     const Lavadero = require("../models/lavadero.js");
+    const Usuario = require("../models/Usuario.js");
+    const usuario1 = new Usuario({
+      nombre: 'Usuario 1',
+      apellido: 'Apellido 1',
+      genero: 'Masculino',
+      fecha_nacimiento: '1990-01-01',
+      correo_electronico: 'slash2130kevin@gmail.com',
+      contrasena: 'contrasena123',
+      telefono: '1234567890',
+      confirmado: true
+    });
+    await usuario1.save();
 
-    const lavadero1 = new Lavadero({
-      nombre: 'Lavadero 1',
-    ciudad: 'Bogota',
-    direccion: 'Calle 1 # 1 - 1',
-    telefono: '1234567890',
-    correo_electronico: 'slash2130kevin@gmail.com',
-    contrasena: 'contrasena123',
-    hora_apertura: '08:00',
-    hora_cierre: '18:00',
-    imagenes: ['https://res.cloudinary.com/djx5h4cjt/image/upload/v1629789853/IMAGENES_LAVADEROS/1_1_1.png'],
-    espacios_de_trabajo: 5,
-    ubicacion: {
-      type: 'Point',
-      coordinates: [-74.08175, 4.60971]
-    },
-    servicios: []
-  });
-
-  lavadero1.confirmado = true;
-
-  await lavadero1.save();
- */
     console.log("Conexión a la base de datos MongoDB exitosa");
   } catch (error) {
     console.error(`Error al conectarse a la base de datos MongoDB: ${error.message}`);
