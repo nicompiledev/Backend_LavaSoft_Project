fa3100b5e6c61891c3c473a18b40048670399bc5
Johnkevin Ortega <slash2130kevin@gmail.com>
2023-05-29 16:45:36 -0500

    Modal servicio

--- models/Servicio.js
+++ models/Servicio.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const servicioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  detalle: { type: String, required: true },
-  costo: { type: Number, required: true },
-  duracion: { type: Number, required: true }
-});
+  const servicioSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    categoria: { type: String, enum: ["lavado", "encerado", "polichado", "aspirado", "desinfeccion", "otros"], required: true },
+    tipoVehiculo: { type: String, required: true },
+    detalle: { type: String, required: true },
+    costo: { type: Number, required: true },
+    duracion: { type: Number, required: true }
+  });
 
 
 const Servicio = mongoose.model("Servicio", servicioSchema);
